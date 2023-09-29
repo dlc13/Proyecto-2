22ce3599e45e02964dbb1a47d78a618712356592
dlc13 <121593372+dlc13@users.noreply.github.com>
2023-09-29 00:35:15 -0300

    Add files via upload

---
+++ Proyecto Integrador Parte 2.py
@@ -0,0 +1,11 @@
+import readchar
+
+while True:
+    key = readchar.readkey()
+    if key == readchar.key.UP:  
+        print("Fin") 
+        break
+    print("Presionaste la tecla: {key}") 
+
+
+
